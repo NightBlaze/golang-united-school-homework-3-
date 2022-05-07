cc37b94425b9260545ddefe0e63300c661639e6d
Alexander Timonenkov <alex@talkatoo.com>
2022-05-07 11:24:47 +0300

    Init result array

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,7 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
+	result = make([]int64, 0)
 	copy(input, result)
 	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
 		result[i], result[j] = result[j], result[i]
