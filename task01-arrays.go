f87f83cdf6646a5d8c433e5e269e2cc9e126833d
Alexander Timonenkov <alex@talkatoo.com>
2022-05-07 11:21:37 +0300

    First commit

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	for _, item := range input {
+		result += item
+	}
+	result /= float32(len(input))
 	return
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+	copy(input, result)
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
 	return
 }
